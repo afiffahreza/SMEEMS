3d4e6a2ae8d7445ee37b2cf7c45884941a010720
afiffahreza <afiffahreza3@gmail.com>
2022-11-16 19:40:29 +0700

    fix user cmds

--- app/bot_handlers/user_cmd.py
+++ app/bot_handlers/user_cmd.py
@@ -8,6 +8,7 @@ def command_help(event):
     text_reply='Available commands:\n'
     text_reply += 'smeems ping\n'
     text_reply += 'smeems help\n'
+    text_reply += 'smeems register <email>\n'
     text_reply += 'smeems smes\n'
     text_reply += 'smeems sme <sme_id>\n'
     text_reply += 'smeems plans <sme_id>\n'
@@ -18,14 +19,15 @@ def command_help(event):
         TextSendMessage(text=text_reply))
 
 def command_register(event, email):
+    profile = line_bot_api.get_profile(event.source.user_id)
     user_data = {
         'id': event.source.user_id,
-        'name': event.source.display_name,
+        'name': profile.display_name,
         'email': email,
         'line_uid': event.source.user_id
     }
     create_user(user_data)
-    text_reply='You have been registered with email: ' + email
+    text_reply='Hi ' + profile.display_name + ', you are now registered with email ' + email
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=text_reply))
